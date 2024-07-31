7d85ccbb57cc576818cf67d70b1bbae83db5ad6d
Sebastiano Quintavalle <878500@stud.unive.it>
2024-07-31 17:20:27 +0200

    chore: minor

--- script/download.py
+++ script/download.py
@@ -6,17 +6,17 @@ from titlequill.utils.io_ import load_json
 from titlequill.utils.logger import Logger
 from titlequill.dataset import OAGKXDownloader
 
-SCRIPT_DIR     = os.path.abspath(os.path.join(__file__, ".."))
-LOCAL_SETTINGS = os.path.join(SCRIPT_DIR, "local_settings.json")
+SCRIPT_DIR     = os.path.abspath(os.path.join(__file__, "..", ".."))
+LOCAL_SETTINGS = os.path.abspath(os.path.join(SCRIPT_DIR, "local_settings.json"))
 
-DATA_DIR = load_json(LOCAL_SETTINGS)["data_dir"]
+DATASET_DIR = load_json(LOCAL_SETTINGS)["dataset_dir"]
 
 
 if __name__ == "__main__":
     
     logger = Logger()
     
-    downloader = OAGKXDownloader(target_dir=DATA_DIR, logger=logger)
+    downloader = OAGKXDownloader(target_dir=DATASET_DIR, logger=logger)
     
     downloader.download()
     
