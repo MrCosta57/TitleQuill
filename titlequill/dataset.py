83d42ecbc94fa6a1da31afadbd4b449db37a86ae
Sebastiano Quintavalle <878500@stud.unive.it>
2024-07-31 17:20:04 +0200

    feat: encoder class

--- titlequill/dataset.py
+++ titlequill/dataset.py
@@ -1,5 +1,6 @@
 
 from dataclasses import dataclass
+import json
 import os
 from typing import Dict, Iterable, List, Tuple
 from urllib.parse import urlparse
@@ -110,6 +111,16 @@ class OAGKXItem:
     def __str__ (self) -> str: return  f'Title: {self.title}\n\nAbstract: {self.abstract}\n\nKeywords: {self.keywords}'
     def __repr__(self) -> str: return str(self)
     
+    @property
+    def item(self) -> Dict:
+        ''' Returns the item as a dictionary for batching '''
+        
+        return {
+            'title'    : self.title,
+            'abstract' : self.abstract,
+            'keywords' : ", ".join(self.keywords) # NOTE: This is made to avoid different length representation in the batch
+        }
+    
     @classmethod
     def from_line(
         cls, 
@@ -121,8 +132,7 @@ class OAGKXItem:
         KEYWORDS_DELIMITER = ' , '
         
         # Parse the line  
-        # TODO This may be dangerous as we are executing an arbitrary line of code, what if a line is  os.rm('/*') ... (:
-        line_dict = eval(line)
+        line_dict = json.loads(line)
         
         # Extract title and abstract
         title    = line_dict['title']
@@ -138,7 +148,7 @@ class OAGKXItem:
         )
 
 
-class OAGKXLoader(Dataset):
+class OAGKXDataset(Dataset):
     '''
     Class to load the OAGKX dataset from the disk and provide an iterable interface to access the items.
     Since the dataset is splitted in chunks, the loader loads the chunks on demand with caching,
@@ -148,7 +158,7 @@ class OAGKXLoader(Dataset):
     PATTERN  = r'part_(\d+)_(\d+)\.txt'
     ''' Pattern to match the file names '''
     
-    def __init__(self, data_dir: str, tokenizer: PreTrainedTokenizer):
+    def __init__(self, data_dir: str, logger: Logger = SilentLogger()):
         '''
         Initialize the loader
 
@@ -158,8 +168,8 @@ class OAGKXLoader(Dataset):
         :type tokenizer: PreTrainedTokenizer
         '''
         
-        self._data_dir  : str                 = data_dir
-        self._tokenizer : PreTrainedTokenizer = tokenizer  # TODO: Not used yet
+        self._data_dir : str    = data_dir
+        self._logger   : Logger = logger
         
         # We represent each file indexes by part and subpart indexes - e.g. part_1_2.txt has indexes (1, 2) 
         # The mapping maps the key to the file absolute path and the interval of indexes in the file to build a global indexing across files
@@ -170,22 +180,54 @@ class OAGKXLoader(Dataset):
         self._loaded_idx = None # this is made to trigger the first load as the current index will of course not match the loaded index
         self._load_chunk(key=first_key)
         
-    def __str__(self) -> str:
-        
-        _, (_, tot_items) = list(self._file_map.values())[-1] 
-        
-        return f'OAGKXLoader[path: {self._data_dir}; files: {len(self._file_map)}; items: {tot_items}]'
-    
+    def __str__(self)  -> str: return f'OAGKXLoader[path: {self._data_dir}; files: {len(self._file_map)}; items: {len(self)}]'
     def __repr__(self) -> str: return str(self)
     
-    def __len__(self) -> int: return len(self._items)
+    def __len__ (self) -> int: 
+        
+        # Use the left index of the last chunk to get the total number of items
+        _, (_, tot_items) = list(self._file_map.values())[-1] 
+        return tot_items
     
-    def __iter__(self) -> Iterable[OAGKXItem]: return iter(self._items)
+    def __iter__(self) -> Iterable[OAGKXItem]: 
+        
+        for idx in range(len(self)): yield self.get_item(idx)
     
-    def __getitem__(self, index: int) -> OAGKXItem: return self._items[index]
+    # NOTE: We create two methods to access the item
+    # - get_item(index) that return the `OAGKXItem` object 
+    # - __getitem__(index), that is the magic method to use the DataLoader as uses the dictionary representation to be loaded in batches
     
+    def get_item(self, idx: int) -> OAGKXItem:
+        ''' 
+        Load the item corresponding to the global index 
+        
+        :param idx: Global index of the item
+        :type idx: int
+        :return: Item at the index
+        :rtype: OAGKXItem
+        '''
+        
+        # Find the chunk key corresponding to the index and load the chunk
+        key = self._find_chunk_key(idx)
+        
+        # Load the chunk
+        self._load_chunk(key)
+        
+        # Get the left relative index in the chunk
+        loaded_from, _ = self._loaded_interval
+        line = self._loaded_lines[idx - loaded_from]
+        
+        return OAGKXItem.from_line(line)
+
+    def __getitem__(self, idx: int) -> Dict[str, str | List[str]] : return self.get_item(idx=idx).item
     
     def _create_file_map(self) -> Dict[Tuple[int, int], Tuple[str, Tuple[int, int]]]:
+        '''
+        Create a mapping from the file indexes to the file path and the interval of indexes in the file
+
+        :return: Mapping from the file indexes to the file path and the interval of indexes in the file
+        :rtype: Dict[Tuple[int, int], Tuple[str, Tuple[int, int]]]
+        '''
         
         def text_to_key(text: str) -> Tuple[int, int]:
             ''' Extracts the indexes from the file name '''
@@ -200,7 +242,8 @@ class OAGKXLoader(Dataset):
             else: 
                 raise ValueError(f"Invalid text format: {text}, expected {self.TEMPLATE} ")
         
-        file_map = {}
+        # Maps part_N1_N2.txt as (N1, N2) -> (file_path, (from, to))
+        file_map: Dict[Tuple[int, int], Tuple[str, Tuple[int, int]]] = {}
         
         prev_idx = 0
         
@@ -208,9 +251,15 @@ class OAGKXLoader(Dataset):
         file_list = sorted(os.listdir(self._data_dir), key=text_to_key)
         
         # Iterate over the files and build the mapping
-        for file_name in tqdm(file_list):
+        self._logger.info(mess=f"Building file map...")
+        
+        progress_bar = tqdm(file_list, desc="Processing files")
+
+        for file_name in progress_bar:
             
             if file_name.endswith('.txt') and re.match(self.PATTERN, file_name):
+                
+                progress_bar.set_description(f"Processing {file_name}")
             
                 N1, N2 = text_to_key(file_name)
                 
@@ -267,25 +316,4 @@ class OAGKXLoader(Dataset):
             if check_interval(idx, interval): return key
         
         raise ValueError(f"Index {idx} out of bounds")
-    
-    def __getitem__(self, idx: int) -> OAGKXItem:
-        ''' 
-        Load the item corresponding to the global index 
-        
-        :param idx: Global index of the item
-        :type idx: int
-        :return: Item at the index
-        :rtype: OAGKXItem
-        '''
-        
-        # Find the chunk key corresponding to the index and load the chunk
-        key = self._find_chunk_key(idx)
-        
-        # Load the chunk
-        self._load_chunk(key)
-        
-        # Get the left relative index in the chunk
-        loaded_from, _ = self._loaded_interval
-        line = self._loaded_lines[idx - loaded_from]
-        
-        return OAGKXItem.from_line(line)
+
--- titlequill/utils/io_.py
+++ titlequill/utils/io_.py
@@ -1,10 +1,19 @@
 import json
+import platform
 import subprocess
-from typing import List
+from typing import Dict, List
 
 
 def read_file_lines(file_path: str) -> List[str]:
-        
+    '''
+    Read lines from a file and return them as a list of strings.
+
+    :param file_path: Path to the file to read
+    :type file_path: str
+    :return: List of lines from the file
+    :rtype: List[str]
+    '''
+    
     lines = []
     
     with open(file_path, 'r') as file:
@@ -12,7 +21,15 @@ def read_file_lines(file_path: str) -> List[str]:
             
     return lines
 
-def load_json(file_path: str) -> dict:
+def load_json(file_path: str) -> Dict:
+    '''
+    Load JSON data from a file and return it as a dictionary
+    
+    :param file_path: Path to the JSON file
+    :type file_path: str
+    :return: JSON data as a dictionary
+    :rtype: Dict
+    '''
     
     with open(file_path, 'r') as file:
         data = json.load(file)
@@ -20,14 +37,37 @@ def load_json(file_path: str) -> dict:
     return data
 
 def get_file_lines_count(file_path: str) -> int:
+    '''
+    Get the number of lines in a file
     
-    # Uncomment in Windows
-    # with open(file_path, 'r') as file:
-    #     count = sum(1 for _ in file)
+    NOTE: The specific implementation depends on the operating system
+        as we are able to specify different os-specific commands to speed-up row count.
     
-    # Uncomment in Linux
-    result = subprocess.run(['wc', '-l', file_path], capture_output=True, text=True)
-    output = result.stdout.strip()
-    count = int(output.split()[0])
+    :param file_path: Path to the file
+    :type file_path: str
+    :return: Number of lines in the file
+    :rtype: int
+    '''
+    
+    # Get the current operating system
+    current_os = platform.system()
+
+    # Match case for different operating systems
+    match current_os:
+        
+        case 'Linux':
+            
+            result = subprocess.run(['wc', '-l', file_path], capture_output=True, text=True)
+            output = result.stdout.strip()
+            count = int(output.split()[0])
+            
+        case 'Windows' | 'Darwin':
+            
+            with open(file_path, 'r') as file:
+                count = sum(1 for _ in file)
+        
+        case _:
+            
+            raise NotImplementedError(f'Unsupported OS: {current_os}')
     
     return count
