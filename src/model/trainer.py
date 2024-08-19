389ea51a20ca593a2b83dac0c25922354caa86d2
Sebastiano Quintavalle <878500@stud.unive.it>
2024-08-19 14:49:27 +0200

    trainer logging and saving history

--- src/model/trainer.py
+++ src/model/trainer.py
@@ -1,25 +1,27 @@
-from typing import Callable, Dict, List, Tuple
+from datetime import datetime
+from typing import Callable, Dict, List, Literal, Tuple
+import loguru
 import torch
 import evaluate
 from functools import partial
 from torch.optim.adamw import AdamW
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
 from datasets import DatasetDict
 from utils.general_utils import postprocess_validation_text
 
 
 class Trainer:
+
     def __init__(
         self,
         model: PreTrainedModel,
         dataset_dict: DatasetDict,
         tokenizer: PreTrainedTokenizerBase,
-        device: str,
+        device: Literal['cpu', 'cuda'],
         train_batch_size: int,
-        train_shuffle: bool,
         val_batch_size: int,
+        shuffle: bool,
         max_length: int,
         max_new_tokens: int,
         max_epochs: int,
@@ -27,38 +29,62 @@ class Trainer:
         loss_fn: Callable,
         lr: float = 1e-5,
         log_interval: int = 100,
-        early_stopping_epochs: int = 5
+        metrics = ['rouge', 'bleu', 'meteor'] 
     ):
-        assert "train" in dataset_dict.keys()
-        assert "validation" in dataset_dict.keys()
-        assert "test" in dataset_dict.keys()
+        
+        assert all([split_name in dataset_dict.keys() for split_name in ["train", "validation", "test"]])
 
+        # Model and Device
         self.device = torch.device(device)
+        self.model = model.to(self.device)  # type: ignore
+
+        # Tokenization
         self.tokenizer = tokenizer
-        self.model = model.to(self.device)
-        self.dataset_dict = dataset_dict
-        self.train_batch_size = train_batch_size
-        self.train_shuffle = train_shuffle
-        self.val_batch_size = val_batch_size
         self.max_length = max_length
         self.max_new_tokens = max_new_tokens
+
+        # Dataset and Dataloader
+        self.dataset_dict = dataset_dict
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+        self.shuffle = shuffle
+
+        # Training loop
         self.max_epochs = max_epochs
+        self.log_interval = log_interval
+
+        # Training callbacks
         self.collate_fn = collate_fn
         self.loss_fn = loss_fn
-        self.log_interval = log_interval
+
+        # Optimizer
         self.optimizer = AdamW(self.model.parameters(), lr=lr)
-        self.rouge_score = evaluate.load("rouge")
-        self.bleu_score = evaluate.load("bleu")
-        self.meteor_score = evaluate.load("meteor")
-        self.early_stopping_epochs = early_stopping_epochs
 
-    def train(self) -> Tuple[PreTrainedModel, Dict[str, List[float]]]:
+        # Metrics
+        self.metrics = {
+            metric_name : evaluate.load(metric_name) for metric_name in metrics
+        }
 
-        self._loss_history = {'train': [], 'val': []}
+    def train(self) -> Tuple[PreTrainedModel, Dict[str, Dict[str, List[float]]]]:
 
+        # Logging function
+        # self.print_fn = print
+        
+        loguru.logger.add(f"logs/training-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log", level="INFO")     
+        self.print_fn = loguru.logger.info
+
+        # Initialize history of losses and metrics
+        self._history = {
+            'train': {'loss': []}, 
+            'val': {metric_name: [] for metric_name in self.metrics}
+        }
+
+        # Set model to training mode
         self.model.train()
+
+        # Initialize metrics
         train_dataloader = DataLoader(
-            self.dataset_dict["train"],
+            self.dataset_dict["train"],  # type: ignore - interface compatibility
             batch_size=self.train_batch_size,
             collate_fn=partial(
                 self.collate_fn,
@@ -66,53 +92,65 @@ class Trainer:
                 max_length=self.max_length,
                 model=self.model,
             ),
-            shuffle=self.train_shuffle,
+            shuffle=self.shuffle,
         )
 
-        print("Starting training...")
+        self.print_fn(f"Starting Training")
+        self.print_fn(f' - Epochs:            {self.max_epochs}')
+        self.print_fn(f' - Train Batch Size:  {self.train_batch_size}')
+        self.print_fn(f' - Num Train Batches: {len(train_dataloader)}')
+        self.print_fn(f' - Device:            {self.device}')
+        self.print_fn(f'')
 
         for epoch in range(self.max_epochs):
 
-            print(f"Epoch {epoch + 1}/{self.max_epochs}")
+            self.print_fn(f"Epoch {epoch + 1}/{self.max_epochs} ({round((epoch + 1) / self.max_epochs * 100, 2)}%)")
 
             loss_batches = []
 
-            for i, batch in enumerate(tqdm(train_dataloader)):
-                # print([f"{k}: {v.shape}" for k, v in batch.items()])
+            for i, batch in enumerate(train_dataloader):
+
+                # Move batch to device
                 batch = {k: v.to(self.device) for k, v in batch.items()}
+
                 # Forward pass
                 outputs = self.model(**batch)
-                loss = self.loss_fn(batch, outputs)
 
-                if i % self.log_interval == 0:
-                    print(f"===Train=== Batch {i} Loss: {loss.item()}")
+                loss = self.loss_fn(batch, outputs)
+                
                 # Backward pass
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-                if i % self.log_interval == 100:
-                    self.print_eval(batch, outputs)
-
+                # Add batch loss
                 loss_batches.append(loss.item())
-            
-            self._loss_history['train'].append(sum(loss_batches) / len(loss_batches))
 
+                # Log
+                if i % self.log_interval == 0:
+                    self.print_fn(f' > Training batch {i+1}/{len(train_dataloader)} ({round(i+1 / len(train_dataloader) * 100, 2)}%) - Loss: {loss.item()}')
+                    self.print_eval(batch, outputs)
+            
+            # Add epoch loss as average of batch losses
+            self._history['train']['loss'].append(sum(loss_batches) / len(loss_batches))
+
+            # Perform validation
             self.validation()
 
-        print("Training completed")
+        self.print_fn("Training completed")
 
         self.model.eval()
 
-        return self.model, self._loss_history
+        return self.model, self._history
 
     @torch.no_grad()
     def validation(self):
 
+        # TODO - Dobbiamo farlo se c'è torch.no_grad()? E se si dobbiamo rimettere self.model.train() alla fine?
         self.model.eval()
 
         val_dataloader = DataLoader(
-            self.dataset_dict["validation"],
+            self.dataset_dict["validation"], # type: ignore - interface compatibility
             batch_size=self.val_batch_size,
             collate_fn=partial(
                 self.collate_fn,
@@ -123,14 +161,19 @@ class Trainer:
             shuffle=False,
         )
 
-        print("Starting validation...")
+        self.print_fn(" Starting validation...")
         for i, batch in enumerate(val_dataloader):
+
+            # Put batch on device
             batch = {k: v.to(self.device) for k, v in batch.items()}
+
+            # Generate predicted tokens
             generated_tokens = self.model.generate(
                 batch["input_ids"],
                 attention_mask=batch["attention_mask"],
                 max_new_tokens=self.max_new_tokens,
             )
+
             labels = batch["labels"]
 
             # Replace -100 in the labels as we can't decode them
@@ -153,28 +196,29 @@ class Trainer:
                 decoded_preds, decoded_labels
             )
 
-            self.rouge_score.add_batch(
-                predictions=decoded_preds, references=decoded_labels
-            )
-            self.bleu_score.add_batch(
-                predictions=decoded_preds, references=decoded_labels
-            )
-            self.meteor_score.add_batch(
-                predictions=decoded_preds, references=decoded_labels
-            )
+            # Add batch to metrics
+            for metric in self.metrics.values():
+                metric.add_batch(predictions=decoded_preds, references=decoded_labels)
 
         # Compute metrics
-        result_rouge = self.rouge_score.compute()
-        result_bleu = self.bleu_score.compute()
-        result_meteor = self.meteor_score.compute()
+        self.print_fn(" > Validation Scores: ")
+        for metric_name, metric in self.metrics.items():
 
-        print("===Validation=== Scores")
-        print(">> ROUGE")
-        print(result_rouge)
-        print(">> BLEU")
-        print(result_bleu)
-        print(">> METEOR")
-        print(result_meteor)
+            result = metric.compute()
+            assert result is not None, f"Error computing metric: {metric_name}"
+
+            match metric_name:
+                case "rouge" : result_log = result['rougeL']
+                case "bleu"  : result_log = result['bleu']
+                case "meteor": result_log = result['meteor']
+                case _       : raise ValueError(f"Invalid metric name: {metric_name}")
+
+            self.print_fn(f"   > {metric_name.upper()}: {result_log}")
+            self._history['val'][metric_name].append(result)
+        
+        self.print_fn("")
+
+        self.model.train()
 
     @torch.no_grad()
     def test(self):
@@ -183,7 +227,11 @@ class Trainer:
 
     @torch.no_grad()
     def print_eval(self, batch, outputs):
+
+        self.model.eval()
+
         if "logits" in outputs:
+
             # For seq2seq models like T5, the logits usually correspond to decoder outputs
             logits = outputs.logits
             predicted_ids = torch.argmax(logits, dim=-1)
@@ -192,9 +240,12 @@ class Trainer:
                 clean_up_tokenization_spaces=False,
                 skip_special_tokens=True,
             )
+
             # Decode the ground truth labels
             if "labels" in batch:
+
                 ground_truth_ids = batch["labels"]
+
                 # Replace -100 in the labels as we can't decode them
                 ground_truth_ids[ground_truth_ids < 0] = self.tokenizer.pad_token_id
                 ground_truth_text = self.tokenizer.batch_decode(
@@ -202,13 +253,21 @@ class Trainer:
                     clean_up_tokenization_spaces=False,
                     skip_special_tokens=True,
                 )
-                for i, zipped in enumerate(zip(predicted_text, ground_truth_text)):
-                    pred, truth = zipped
-                    print(f"First example batch")
-                    print(f"Prediction:\n{pred}")
-                    print(f"Ground Truth:\n{truth}")
-                    break
+
+                # Print the first example and the one in the middle
+                # NOTE: This is mostly for the two-task training
+                log_idx = [0, (len(predicted_text) + 1) // 2]
+
+                for idx in log_idx:
+
+                    self.print_fn(f" > Example {idx} in the batch")
+                    self.print_fn(f"   > Prediction:   {predicted_text[idx]}")
+                    self.print_fn(f"   > Ground Truth: {ground_truth_text[idx]}")
+                    self.print_fn("")
+                
             else:
-                print("No labels found in batch. Check the batch structure.")
+                self.print_fn("No labels found in batch. Check the batch structure.")
         else:
-            print("No logits found in model output. Check the model output structure.")
+            self.print_fn("No logits found in model output. Check the model output structure.")
+
+        self.model.train()
--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 from functools import partial
+import os
+import pickle
 import torch
 from torch.utils.data import DataLoader
 from transformers import (
@@ -26,7 +28,7 @@ TRAINING_STRATEGIES = {
     ),
     'divided_tasks': (
         partial(custom_collate_seq2seq_2task, shuffle=False),
-        twotasks_ce_loss_fn
+        hf_loss_fn #twotasks_ce_loss_fn
     ),
     'divided_tasks_shuffle': (
         partial(custom_collate_seq2seq_2task, shuffle=True),
@@ -39,14 +41,16 @@ def main():
     """if cfg.use_wandb:
     wandb.require("core")
     wandb.login(key=os.getenv("WANDB_API_KEY"))"""
+
+    OUT_DIR = "output"
     
-    STRATEGY = 'combined_tasks'
+    STRATEGY = 'divided_tasks'
     assert STRATEGY in TRAINING_STRATEGIES, f"Invalid training strategy: {STRATEGY}. Choose one of {list(TRAINING_STRATEGIES.keys())}"
     
     collate_fn, loss_fn = TRAINING_STRATEGIES[STRATEGY]
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    num_epochs = 100
+    num_epochs = 1
     model_type = "google/flan-t5-small"
     max_length = 512
     max_new_tokens = 150
@@ -57,6 +61,7 @@ def main():
     model = AutoModelForSeq2SeqLM.from_pretrained(model_type)
     dataset_dict = load_oagkx_dataset(
         data_dir=data_dir, 
+        split_size=(0.75, 0.1, 0.1),
         just_one_file=True, 
         filter_fn=filter_on_stats
     )
@@ -69,18 +74,29 @@ def main():
         device=device,
         max_epochs=num_epochs,
         dataset_dict=dataset_dict,
-        train_batch_size=64,
-        train_shuffle=True,
-        val_batch_size=64,
+        train_batch_size=4,
+        shuffle=True,
+        val_batch_size=4,
         max_length=max_length,
         max_new_tokens=max_new_tokens,
         collate_fn=collate_fn,
         loss_fn=loss_fn,
-        log_interval=10,
+        log_interval=500,
         lr=1e-5,
     )
 
-    trainer.train()
+    model, history = trainer.train()
+
+    os.makedirs(OUT_DIR, exist_ok=True)
+
+    model_out = os.path.join(OUT_DIR, f"model_{STRATEGY}.pt")
+    print(f"Saving model and history to {model_out}")
+    torch.save(model.state_dict(), model_out)
+
+    history_out = os.path.join(OUT_DIR, f"history_{STRATEGY}.pkl")
+    print(f"Saving model and history to {history_out}")
+    with open(history_out, "wb") as f:
+        pickle.dump(history, f)
 
 
 if __name__ == "__main__":
