7e4f65d73d1b503513c709f4a9e96215cff32d1e
NicolaAggio <880008@stud.unive.it>
2024-08-25 11:23:10 +0200

    Baseline implementation, update Evaluatore

---
+++ src/run_baseline.py
@@ -0,0 +1,54 @@
+from utils.evaluator import Evaluator
+from datamodule.dataset import OAGKXItemStats
+from tqdm import tqdm
+from datamodule.dataset import load_oagkx_dataset
+
+eval_first_baseline = Evaluator()
+eval_second_baseline = Evaluator()
+eval_keywords_baseline = Evaluator()
+
+data_dir = "/Users/nicolaaggio/Desktop/UNI/TitleQuill/data/OAGKX"
+
+def main():
+    dataset = load_oagkx_dataset(
+        data_dir,
+        (.7,.15,.15),
+        just_one_file=True,
+        verbose=True)
+    
+    for entry in tqdm(dataset['test'], desc="Evaluating Baseline"):
+        item = OAGKXItemStats.from_json(entry)
+
+        true_title = item.title
+        true_keywords = item.keywords
+
+        first_baseline_title = item.sentence_with_more_keywords[0]
+        second_baseline_title = item.abstract_first_sentence
+        baseline_keywords = set(item.get_most_frequent_words().keys())
+
+        labels = sorted(list(baseline_keywords.union(true_keywords)))
+
+        # Convert lists to binary format
+        pred_binary = [1 if label in baseline_keywords else 0 for label in labels]
+        ref_binary = [1 if label in true_keywords else 0 for label in labels]
+
+        eval_first_baseline.add_batch_title([first_baseline_title], [true_title])
+        eval_second_baseline.add_batch_title([second_baseline_title], [true_title])
+        eval_keywords_baseline.add_batch_keywords(pred_binary, ref_binary)
+
+    log_title_first = eval_first_baseline.compute_title()
+    log_title_second = eval_second_baseline.compute_title()
+    log_keywords = eval_keywords_baseline.compute_keywords()
+
+    for metric_name, result in log_title_first.items():
+            print(f"Title - First Baseline   > {metric_name.upper()}: {result}")
+
+    for metric_name, result in log_title_second.items():
+            print(f"Title - Second Baseline   > {metric_name.upper()}: {result}")
+
+    for metric_name, result in log_keywords.items():
+            print(f"Keywords   > {metric_name.upper()}: {result}")
+    
+
+if __name__ == "__main__":
+    main()
--- src/utils/evaluator.py
+++ src/utils/evaluator.py
@@ -2,22 +2,38 @@ import evaluate
 
 
 class Evaluator:
-    def __init__(self, metrics=["rouge", "bleu", "meteor"]):
-        self.metrics = {
-            metric_name: evaluate.load(metric_name) for metric_name in metrics
+    def __init__(self, 
+                metrics_title=["rouge", "bleu", "meteor"], 
+                metrics_keywords=["f1", "precision", "recall"]): # jaccard missing
+
+        self.metrics_title = {
+            metric_name: evaluate.load(metric_name) for metric_name in metrics_title
+        }
+
+        self.metrics_keywords = {
+            metric_name: evaluate.load(metric_name) for metric_name in metrics_keywords
         }
 
     @property
-    def get_metrics(self):
-        return self.metrics
+    def get_metrics_title(self):
+        return self.metrics_title
 
-    def add_batch(self, predicted, target):
-        for metric in self.metrics.values():
+    @property
+    def get_metrics_keywords(self):
+        return self.metrics_keywords
+
+    def add_batch_title(self, predicted, target):
+        for metric in self.metrics_title.values():
             metric.add_batch(predictions=predicted, references=target)
 
-    def compute(self):
+    def add_batch_keywords(self, predicted, target):
+        for metric in self.metrics_keywords.values():
+            metric.add_batch(predictions=predicted, references=target)
+
+
+    def compute_title(self):
         result_log = {}
-        for metric_name, metric in self.metrics.items():
+        for metric_name, metric in self.metrics_title.items():
             result = metric.compute()
             assert result is not None, f"Error computing metric: {metric_name}"
             match metric_name:
@@ -27,6 +43,24 @@ class Evaluator:
                     result_log[metric_name] = result["bleu"]
                 case "meteor":
                     result_log[metric_name] = result["meteor"]
+                case _:
+                    raise ValueError(f"Invalid metric name: {metric_name}")
+        return result_log
+
+    def compute_keywords(self):
+        result_log = {}
+        for metric_name, metric in self.metrics_keywords.items():
+            result = metric.compute()
+            assert result is not None, f"Error computing metric: {metric_name}"
+            match metric_name:
+                case "f1":
+                    result_log[metric_name] = result["f1"]
+                case "precision":
+                    result_log[metric_name] = result["precision"]
+                case "recall":
+                    result_log[metric_name] = result["recall"]
+                # case "jaccard":
+                #     result_log[metric_name] = result["jaccard"]
                 case _:
                     raise ValueError(f"Invalid metric name: {metric_name}")
         return result_log
